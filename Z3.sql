ae7764e5a8b113e39ffcddf57767ef36a4ba74f7
filipkobus <fkobus.coding@gmail.com>
2024-05-23 11:59:50 +0200

    Update and rename Z3_331703.sql to Z3.sql

--- Z3.sql
+++ Z3.sql
@@ -2,12 +2,12 @@
 
 /*
 Z3.1
-pokazaæ osoby które z województwa (wybraæ)
-które nigdy nie pracowa³y w firmie zawieraj¹cej w nazwie s³owo Politechnika (gdziekolwiek)
+pokazaÄ‡ osoby ktÃ³re z wojewÃ³dztwa (wybraÄ‡)
+ktÃ³re nigdy nie pracowaÅ‚y w firmie zawierajÄ…cej w nazwie sÅ‚owo Politechnika (gdziekolwiek)
 */
 
 
-SELECT LEFT(o.IMIÊ, 20) AS IMIÊ, LEFT(o.NAZWISKO, 20) AS NAZWISKO
+SELECT LEFT(o.IMIÄ˜, 20) AS IMIÄ˜, LEFT(o.NAZWISKO, 20) AS NAZWISKO
 	FROM osoby o
 	JOIN miasta m ON (o.id_miasta = m.id_miasta)
 	JOIN woj w ON (w.kod_woj = m.kod_woj)
@@ -21,22 +21,22 @@ SELECT LEFT(o.IMIÊ, 20) AS IMIÊ, LEFT(o.NAZWISKO, 20) AS NAZWISKO
 	)
 
 /*
-IMIÊ                 NAZWISKO
+IMIÄ˜                 NAZWISKO
 -------------------- --------------------
 Katarzyna            Kowalczyk
 Piotr                Nowakowski
 Jakub                Piotrowski
-Micha³               Gierczak
+MichaÅ‚               Gierczak
 
 (4 row(s) affected)
 */
 
 /*
 Z3.2
-Pokazaæ osoby które nigdy nie mia³y etaty z pensj¹ powy¿ej (wybraæ kwotê) w firmach z województwa (wybraæ)
+PokazaÄ‡ osoby ktÃ³re nigdy nie miaÅ‚y etaty z pensjÄ… powyÅ¼ej (wybraÄ‡ kwotÄ™) w firmach z wojewÃ³dztwa (wybraÄ‡)
 */
 
-SELECT LEFT(o.IMIÊ, 20) AS IMIÊ, LEFT(o.NAZWISKO, 20) AS NAZWISKO
+SELECT LEFT(o.IMIÄ˜, 20) AS IMIÄ˜, LEFT(o.NAZWISKO, 20) AS NAZWISKO
 	FROM osoby o
 	WHERE NOT EXISTS
 	(	SELECT 1
@@ -50,18 +50,18 @@ SELECT LEFT(o.IMIÊ, 20) AS IMIÊ, LEFT(o.NAZWISKO, 20) AS NAZWISKO
 	)
 
 /*
-IMIÊ                 NAZWISKO
+IMIÄ˜                 NAZWISKO
 -------------------- --------------------
 Jan                  Kowalski
 Anna                 Nowak
 Katarzyna            Kowalczyk
-Marek                Wójcik
-Magdalena            Kamiñska
+Marek                WÃ³jcik
+Magdalena            KamiÅ„ska
 Piotr                Nowakowski
 Jakub                Piotrowski
 Marcin               Kochalski
 Karolina             Gwaiazdowski
-Micha³               Gierczak
+MichaÅ‚               Gierczak
 Mateusz              Kucharczyk
 
 (11 row(s) affected)
@@ -69,11 +69,11 @@ Mateusz              Kucharczyk
 
 /*
 Z3.3
-poszukaæ najmniejsz¹ pensjê w bazie i pokazaæ w jakiej firmie i jaka osoba posiada
+poszukaÄ‡ najmniejszÄ… pensjÄ™ w bazie i pokazaÄ‡ w jakiej firmie i jaka osoba posiada
 */
 
 SELECT 
-    LEFT(o.IMIÊ, 20) AS IMIÊ, 
+    LEFT(o.IMIÄ˜, 20) AS IMIÄ˜, 
     LEFT(o.NAZWISKO, 20) AS NAZWISKO, 
     LEFT(f.nazwa, 20) AS firma, 
     LEFT(e.stanowisko, 20) AS stanowisko, 
@@ -88,16 +88,16 @@ WHERE
     e.pensja = (SELECT MIN(LEFT(ew.pensja, 20)) FROM etaty ew)
 
 /*
-IMIÊ                 NAZWISKO             firma                stanowisko           pensja
+IMIÄ˜                 NAZWISKO             firma                stanowisko           pensja
 -------------------- -------------------- -------------------- -------------------- -----------
-Micha³               Wiœniewski           Quantum Dynamics     Asystent             3500
+MichaÅ‚               WiÅ›niewski           Quantum Dynamics     Asystent             3500
 
 (1 row(s) affected)
 */
 
 /*
 Z3.4
-stworzyæ tabelkê XX z kolumn¹ YY nvarchar(100) not null pod warunkiem ze takowej nie ma
+stworzyÄ‡ tabelkÄ™ XX z kolumnÄ… YY nvarchar(100) not null pod warunkiem ze takowej nie ma
 */
 
 IF NOT EXISTS (
@@ -115,7 +115,7 @@ GO
 
 /*
 Z3.5
-Dodaæ kolummê ZZ bit not null default 0 do tabelki XX pod warunkiem, ze w tej tabelce takiej kolumny jeszcze nie ma
+DodaÄ‡ kolummÄ™ ZZ bit not null default 0 do tabelki XX pod warunkiem, ze w tej tabelce takiej kolumny jeszcze nie ma
 */
 
 IF NOT EXISTS (
